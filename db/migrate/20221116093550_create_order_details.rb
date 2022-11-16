4bd7375dc3f767866aa9921debbaba7f89a27c60
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-16 10:01:16 +0000

    Add Model

---
+++ db/migrate/20221116093550_create_order_details.rb
@@ -0,0 +1,14 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+
+      t.integer :order_id, null: false
+      t.integer :making_status, null: false
+      t.integer :product_id, null: false
+      t.integer :quantity, null: false
+      t.integer :tax_price, null: false
+      
+      t.timestamps
+    end
+  end
+end
