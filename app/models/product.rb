aa64099cac7d15e3771c00fa8ea63006a8e4297f
KatsuyaHiraki <112613868+hirakikatsuya@users.noreply.github.com>
2022-11-16 10:47:26 +0000

    Product model追加

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+
+  has_one_attached :product_image
+
+  def get_product_image
+    unless product_image.attached?
+      file_path=Rails.root.join('app/assets/images/no_cake_image.jpg')
+      product_image.attach(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+    end
+  end
+
+end
