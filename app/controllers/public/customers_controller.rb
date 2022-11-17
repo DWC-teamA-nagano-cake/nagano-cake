8425cf203f657ca2a7bd8ed950f00bd9019c77e4
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-17 14:12:56 +0000

    fix detail controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,30 +1,3 @@
 class Public::CustomersController < ApplicationController
-  # before_action :authenticate_admin!
-  def index
-    @cutomers = Customer.all.page(params[:page]).per(10)
-  end
   
-  def show
-    @cutomer = Customer.find(params[:id])
-    
-  end
-  
-  def edit
-    @customer = Customer.find(params[:id])
-  end
-  
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to admin_customer_path(@customer)
-    else
-      redirect_to request.referer
-    end
-  end
-  
-  private
-  
-  def customer_params
-    params.require(:cutomer).permit(:last_name, :first_name, :last_name_kana, :post_code, :address, :phone_number, :email, :is_deleted)
-  end
-end
+ 
