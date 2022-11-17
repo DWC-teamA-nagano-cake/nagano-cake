e89722c34457d4adcc7643f8af64d9a047b59d90
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-11-17 09:28:33 +0000

    admin products genres

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,28 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.page(params[:page])
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to admin_products
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :is_active, :name, :description, :price)
+  end
+
 end
