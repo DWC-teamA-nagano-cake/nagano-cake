53b22a3ac0004be20ad07df2f2fce05364571050
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-17 09:13:58 +0000

    create index and write code all

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,2 +1,9 @@
 class Admin::HomesController < ApplicationController
+  # before_action :authenticate_admin!
+  
+  def top
+    @orders = Order.all
+    @order_new = Order.new
+    @order_details = OrderDetail.all 
+  end
 end
--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,11 @@
 class Admin::OrderDetailsController < ApplicationController
+  
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "production"
+      @order_detail.order.update(order_status: 2)
+    elsif OrderDetail.where(order_status :3)
+    
+  end
 end
