7fe65548fcaf7da6dd89ae76280fff85ab19db2f
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-18 11:05:28 +0000

    create_cart_items

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,12 +1,12 @@
 class Public::ProductsController < ApplicationController
 
   def index
-    @products=Product.all.page(params[:page]).per(8)
+    @products = Product.all.page(params[:page]).per(8)
   end
 
   def show
-    @product=Product.find(params[:id])
-    @cart_item=CartItem.new
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,19 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+
+  validates :quantity, presence: true
+
+  def sub_total
+    quantity*product.with_tax_price
+  end
+
+  def quantity_options
+    options = []
+    (self.quantity + 11).times do |count|
+      options << [count, count]
+    end
+    return options
+  end
+
 end
