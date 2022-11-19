b7ddb978121fa586de413a6cd3ad4e8fca3b8606
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-19 06:46:53 +0000

    create_genres_search

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -2,11 +2,20 @@ class Public::ProductsController < ApplicationController
 
   def index
     @products = Product.all.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
     @product = Product.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
+  end
+  
+  def searches
+    @genres = Genre.all
+    @search_name = params[:genre_name]
+    @products_all = Genre.where(@search_name)
+    @products = @products_all.page(params[:page]).per(8)
   end
 
 end
