fb74f24115479d3cba94ccb7a2d106670f3bc06e
KatsuyaHiraki <112613868+hirakikatsuya@users.noreply.github.com>
2022-11-19 05:45:06 +0000

    退会機能実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,10 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
@@ -12,8 +12,13 @@ class Customer < ApplicationRecord
   validates :post_code, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-         
+
   has_many :shipping_addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
+
+  def active_for_authentication?
+    super && (is_deleted==false)
+  end
+
 end
