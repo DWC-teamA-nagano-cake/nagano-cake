6d773778ba970ef85900d8e1de489340f24bd581
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-19 10:20:42 +0000

    fix detail

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,12 +5,14 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail.update(order_detail_params)
     if @order_detail.making_status == "production"
       @order_detail.order.update(order_status: 2)
-    elsif OrderDetail.where(maiking_status: "completed", order_id: @order_detail.order_id).count == @order.order_details.count
+    elsif OrderDetail.where(maiking_status: "completed", order_id: @order_detail.order_id).count == @order_detail.order.order_details.count
       @order_detail.order.update(order_status: 3)
     end
     redirect_to request.referer
   end
 
   def order_detail_params
-    params.require(:order_detail).permit(:making_status: 3)
+    params.require(:order_detail).permit(:making_status)
+  end
+  
 end
