7a67a8d0d279c674fb3f6045edabb52a89827af6
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-20 03:45:53 +0000

    fix detail

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,10 +5,10 @@ class Order < ApplicationRecord
   enum method_of_payment: {credit_card: 0, transfer: 1}
   enum order_status: {waiting: 0, confirmation: 1, production: 2, preparing: 3 , shipped: 4}
 
-  # def address_display　おそらく不要
-  #   '〒' + post_code + ' ' + address + ' ' + last_name + ' ' + first_name
-  # end
-  
+  def address_display
+    '〒' + post_code + ' ' + address + ' ' + name
+  end
+
 end
 
 
