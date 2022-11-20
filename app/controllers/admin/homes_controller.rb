d52ec310a2dbe4f7e63cffcb77c4c90972ba67d5
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-20 14:26:23 +0000

    fix none

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,7 +3,6 @@ class Admin::HomesController < ApplicationController
 
   def top
     @orders = Order.page(params[:page])
-    # @order_new = Order.new
-    # order_details = order.order_details
+    
   end
 end
