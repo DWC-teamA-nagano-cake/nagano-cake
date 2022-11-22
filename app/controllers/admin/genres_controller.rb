db25f45dce87ee2da00b37978ca15ce6f65abfda
EC2 Default User <dd.flash85@gmail.com>
2022-11-22 03:51:23 +0000

    Fix genres_controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -15,7 +15,7 @@ class Admin::GenresController < ApplicationController
   def create
     genre = Genre.new(genre_params)
     if genre.save
-      redirect_to admin_genres
+      redirect_to admin_genres_path
     end
   end
 
