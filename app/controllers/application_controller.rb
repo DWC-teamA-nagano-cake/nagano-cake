c7cad57c13cc2d6ee3c2fb503163b336c18a2ebf
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-22 09:09:24 +0000

    検索機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-
-
-  protected
+ private
 
   def set_search
-    @q = Customer.ransack(params[:q])
-    @customers = @q.result(distinct: true).select('customer.*, first_name, last_name').page(params[:page])
-    # @customers = @q.result.includes(:first_name).page(params[:page]).to_a.uniq
-  
+    @q = Product.ransack(params[:q])
+    @product = @q.result(distinct: true).page(params[:page]).per(8)
+    # @product = @q.result.includes(:name).page(params[:page]).to_a.uniq
+
   end
 #   ransackメソッドは検索ヘルパーメソッドである
 # resultメソッドは検索結果を返すヘルパーメソッドである
 # distinct: trueは、重複する検索結果を除外する役割を持つ
+
+ protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number])
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,12 +1,11 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
     @products = Product.limit(4).order("created_at DESC")
     @genres = Genre.all
   end
-  
+
   def about
   end
-  
 
 end
