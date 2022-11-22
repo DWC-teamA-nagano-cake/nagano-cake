04c38b63a6e52bdf322cd7b381ec348cdf4a4834
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-22 09:40:21 +0000

    余分な記述の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def set_search
     @q = Product.ransack(params[:q])
-    @product = @q.result(distinct: true).page(params[:page]).per(8)
+    @product = @q.result(distinct: true)
     # @product = @q.result.includes(:name).page(params[:page]).to_a.uniq
 
   end
