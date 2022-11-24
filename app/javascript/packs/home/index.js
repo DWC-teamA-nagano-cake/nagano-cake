6acd852bf8a0a271994ee74f9ccc41afced3f2c5
Nagisa Kawashima <nevergiveup0922@gmail.com>
2022-11-24 01:37:09 +0000

    fix layout blank

---
+++ app/javascript/packs/home/index.js
@@ -0,0 +1,24 @@
+
+
+  // $(document).click(function() {
+  //     const icon = document.getElementById('icon');
+  //     const close = document.getElementById('close');
+
+  //     close.addEventListener('click', () => {
+  //       icon.classList.add('hide');
+  //     });
+  //   });
+
+
+
+
+// window.onload = function () {
+
+//   const icon = document.getElementById('icon');
+//   const close = document.getElementById('close');
+
+//   close.addEventListener('click', () => {
+//     icon.classList.add('hide');
+//   });
+
+// };
